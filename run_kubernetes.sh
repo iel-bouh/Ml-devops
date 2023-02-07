c1620f3fef520c1efa47ada77a033d94f642db2a
Ismail El bouhi <elbouhiismail69@gmail.com>
2023-02-07 20:58:27 +0100

    complete k8 script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,23 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=smailbou/app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl apply -f deployment.yml
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
+pod_name=$(kubectl get pods --selector=app=my-app --output=jsonpath='{.items[0].metadata.name}')
+kubectl port-forward $pod_name 8000:80
+
+
+
 
